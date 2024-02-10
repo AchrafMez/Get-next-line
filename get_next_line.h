3c88394ba6847973881256940c1f01b836c0ef14
Achraf Meziouni <afmz414s@gmail.com>
2024-02-10 16:21:19 +0100

    test

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,18 @@
 /*   By: amezioun <amezioun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 10:24:44 by amezioun          #+#    #+#             */
-/*   Updated: 2024/02/06 10:24:45 by amezioun         ###   ########.fr       */
+/*   Updated: 2024/02/10 12:16:06 by amezioun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// #ifndef GET_NEXT_LINE_H
+// # define GET_NEXT_LINE
+// #ifndef BUFFER_SIZE
+// #define BUFFER_SIZE 10
+// #endif
+
+// char *get_next_line(int fd);
+
+
+
+// #endif
--- gnl.c
+++ gnl.c
@@ -6,7 +6,7 @@
 /*   By: amezioun <amezioun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 10:22:52 by amezioun          #+#    #+#             */
-/*   Updated: 2024/02/09 21:32:10 by amezioun         ###   ########.fr       */
+/*   Updated: 2024/02/10 15:18:53 by amezioun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,37 +52,72 @@ char *ft_strdup(const char *s, int size)
         i++;
     }
     result[i] = '\0';
+    free(result);
     return result;
 }
 char *ft_line(char *s, int start, int l7ed)
 {
-    int size = l7ed - start;
-    // char *line = malloc(size + 1);
-    char *line;
-    int i = 0;
+
     if(!s)
         return NULL;
+    int i = 0;
+    int size = l7ed - start;
+    char *line = malloc(l7ed + 1);
     if(start > l7ed)
         return NULL;
-    while (start < l7ed)
+    while (s && s[i] != '\0' && start < l7ed)
     {
         line[i] = s[i];
         i++;
     }
-    // line[size - 1] = '\n';
-    // line[size] = '\0';
+    line[size - 1] = '\n';
+    line[size] = '\0';
     return s;
 }
+
+
+
+char *ft_substr(char const *s, unsigned int start, size_t len)
+{
+    int i = 0;
+    char *result;
+    int size = len - start;
+    if(!s)
+        return NULL;
+    result = malloc(size + 2);
+    if(!result)
+        return NULL;
+    while (s && s[i] != '\0' && i < size)
+    {
+        result[i] = s[start + i];
+        i++;
+    }
+    result[i] = '\0';
+    result[i + 1] = '\n';
+    return result;
+}
+
 char *get_next_line(int fd)
 {
-    char buffer[BUFFER_SIZE];
+    int i = 0;
+    static char *s;
+    char buffer[BUFFER_SIZE + 1];
     char *line;
-    int bytes_read = read(fd, buffer, 10);
-    char *result = ft_strdup(buffer, bytes_read);
-    int l7ed = ft_check(result);
-    // line = ft_line(result, 1, l7ed);
-    
-    return result;
+    int bytes_read = read(fd, buffer, BUFFER_SIZE);
+    buffer[bytes_read] = '\0';
+    // char *result = ft_strdup(buffer, bytes_read);
+    line = malloc(bytes_read + 1);
+    if(!line)
+        return NULL;
+    while(buffer[i] != '\0')
+    {
+        line[i] = buffer[i];
+        i++;
+    }
+    int l7ed = ft_check(line);
+    s = ft_substr(line, 0, l7ed);
+    // printf("%d\n", bytes_read);
+    return s;
 }
 
 int main()
@@ -91,6 +126,7 @@ int main()
     if(fd == -1)
         printf("fd error");
     printf("%s", get_next_line(fd));
-    //get_next_line(fd);
     // get_next_line(fd);
+    // get_next_line(fd);
+    printf("%s", get_next_line(fd));
 }
